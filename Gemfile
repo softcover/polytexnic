af6d49714484d27b4e7453b40d7717665956dd44
Michael Hartl <michael@michaelhartl.com>
2013-02-11 16:34:58 -0800

    [#44239947] Working call to tralics (with tempfile)

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rspec', '~> 2.12.0'
+gem 'nokogiri', '~> 1.5.0'
 
 # Specify your gem's dependencies in polytexnic-core.gemspec
 gemspec
--- lib/polytexnic-core.rb
+++ lib/polytexnic-core.rb
@@ -1,9 +1,21 @@
 require "polytexnic-core/version"
+require 'tempfile'
+require 'open3'
+require 'nokogiri'
 
 module Polytexnic
   module Core
     def self.polytex_to_html_fragment(polytex)
-      xml_to_html(polytex)
+      tralics = `which tralics`.strip
+      file = Tempfile.new(['polytex', '.tex'])
+      file.write(polytex)
+      file.close
+      system("#{tralics} -nomathml #{file.path} > /dev/null")
+      dirname = File.dirname(file.path)
+      xml_filename = File.basename(file.path, '.tex') + '.xml'
+      xml_to_html(File.read(File.join(dirname, xml_filename)))
+    ensure
+       file.unlink
     end
 
     def self.xml_to_html(xml)
