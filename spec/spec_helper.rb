d6140207bc628391894ceb5eade41228e12d828a
Michael Hartl <michael@michaelhartl.com>
2013-02-14 14:44:40 -0800

    [Delivers #44492369] Move resemble matcher to a support file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,16 @@
 require 'polytexnic-core'
 
+# Load support files.
+Dir.glob(File.join(File.dirname(__FILE__), "./support/**/*.rb")).each do |f|
+  require_relative(f)
+end
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
+  # Disallow the old-style 'object.should' syntax.
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
@@ -14,26 +20,4 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
-end
-
-RSpec::Matchers.define :resemble do |expected|
-  match do |actual|
-    expect(actual.compress).to match_regex(expected.to_s.compress)
-  end
-end
-
-class String
-
-  # Compress whitespace
-  # Eliminates repeating whitespace (apart from newlines)
-  # >> "foo\t    bar\n\nbaz    quux\nderp".compress
-  # => "foo bar\n\nbaz quux\nderp"
-  def compress
-    self.strip.gsub(/[ \t]{2,}/, ' ')
-  end
-
-  def compress!
-    replace(self.compress)
-  end
-
 end
