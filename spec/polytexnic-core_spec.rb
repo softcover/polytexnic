dfa04d88e17e1e7f3421d97c4c5a7381c4f9b251
Michael Hartl <michael@michaelhartl.com>
2013-02-15 17:59:52 -0800

    [Delivers #44578639] Improve 'resembles' multiline matching

--- spec/polytexnic-core_spec.rb
+++ spec/polytexnic-core_spec.rb
@@ -70,8 +70,7 @@ describe Polytexnic::Core do
         end        
 
         it "should raise an error" do
-          expect { subject }.to raise_error
-        end
+          expect { subject }.to raise_error end
       end
     end
   end
