6b39377ea4b489e7f5da1c840c16bbf8eb3098bb
Nick Merwin <nick@lemurheavy.com>
2013-02-26 15:34:21 -0800

    [#44592469] split core into post/preprocessor; added Core::Pipeline class

--- spec/polytexnic-core_spec.rb
+++ spec/polytexnic-core_spec.rb
@@ -1,11 +1,9 @@
 # encoding=utf-8
 require 'spec_helper'
 
-describe Polytexnic::Core do
-  it { should respond_to(:polytex_to_html_fragment) }
-
-  describe '#polytex_to_html' do
-    subject { Polytexnic::Core.polytex_to_html_fragment(polytex) }
+describe Polytexnic::Core::Pipeline do
+  describe '#process' do
+    subject { Polytexnic::Core::Pipeline.new(polytex).process }
 
     describe "italics conversion" do
       let(:polytex) { '\emph{foo bar}' }
