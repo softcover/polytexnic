9723e603f0ad586e1379ec012d2ecec0daac11a2
Michael Hartl <michael@michaelhartl.com>
2013-03-28 16:58:00 -0800

    [Delivers #47037001] Add math_environments integration test and refactor

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,22 +1,31 @@
 # encoding=utf-8
 require 'spec_helper'
 
+# Returns a list of fixture filenames.
+def filenames
+  %w[inline_math verbatim_environments math_environments]    
+end
+
+# Returns the results of converting the TeX filename to HTML.
+def converted(filename)
+  Polytexnic::Core::Pipeline.new(contents(filename, 'tex')).process
+end
+
+# Returns the contents of the HTML filename fixture.
+def html(filename)
+  contents(filename, 'html')
+end
+
+# Returns the contents of a filename using its extension.
+def contents(filename, extension)
+  File.open(File.join('spec', 'fixtures', "#{filename}.#{extension}")).read
+end
+
 describe Polytexnic::Core::Pipeline do
-  %w[inline_math verbatim_environments].each do |filename|
+  
+  filenames.each do |filename|
     it "should correctly process #{filename}" do
-      expect(converted(filename)).to eql(html(filename))
+      expect(converted(filename)).to resemble(html(filename))
     end
-  end
-
-  def converted(filename)
-    Polytexnic::Core::Pipeline.new(contents(filename, 'tex')).process
-  end
-
-  def html(filename)
-    contents(filename, 'html')
-  end
-
-  def contents(filename, extension)
-    File.open(File.join('spec', 'fixtures', "#{filename}.#{extension}")).read
   end
 end
