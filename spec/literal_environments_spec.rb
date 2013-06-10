6c114f0e50f92c9eb7ba97d89ab3381e47bc76ae
Michael Hartl <michael@michaelhartl.com>
2013-06-10 16:16:44 -0700

    Factor code into spec
    [#51360619]

--- spec/literal_environments_spec.rb
+++ spec/literal_environments_spec.rb
@@ -5,42 +5,6 @@ describe Polytexnic::Core::Pipeline do
   let(:processed_text) { Polytexnic::Core::Pipeline.new(polytex).to_html }
   subject { processed_text }
 
-  describe "code blocks" do
-    describe "without syntax highlighting" do
-      let(:polytex) do <<-'EOS'
-        \begin{code}
-        def foo
-          "bar"
-        end
-        \end{code}
-        EOS
-      end
-
-      it { should resemble 'def foo' }
-      it { should resemble '<div class="code">' }
-      it { should_not resemble '\begin{code}' }
-    end
-
-    describe "with syntax highlighting" do
-      let(:polytex) do <<-'EOS'
-        %= lang:ruby
-        \begin{code}
-        def foo
-          "bar"
-        end
-        \end{code}
-        EOS
-      end
-
-      it { should resemble '<div class="code">' }
-      it "should not have repeated code divs" do
-        expect(processed_text.scan(/<div class="code">/).length).to eq 1
-      end
-      it { should resemble '<div class="highlight">' }
-      it { should resemble '<pre>' }
-    end
-  end
-
   describe "non-ASCII Unicode" do
     let(:polytex) { 'Алексей Разуваев' }
     it { should include(%(<span class="unicode">Алексей</span>)) }
