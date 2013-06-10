3a3e118fa3320acb42a6dad53c6f50e9241ac574
Michael Hartl <michael@michaelhartl.com>
2013-06-10 16:07:14 -0700

    Factor math into spec
    [#51360619]

---
+++ spec/to_html/math_spec.rb
@@ -0,0 +1,207 @@
+# encoding=utf-8
+require 'spec_helper'
+
+describe Polytexnic::Core::Pipeline do
+  let(:processed_text) { Polytexnic::Core::Pipeline.new(polytex).to_html }
+  subject { processed_text }
+
+  describe "inline math" do
+
+    context "LaTeX-style" do
+      let(:equation) do <<-'EOS'
+        \( \int_\Omega d\omega = \int_{\partial\Omega} \omega \)
+        EOS
+      end
+      let(:polytex) { equation }
+      let(:contents) { '\Omega' }
+
+      it { should resemble contents }
+      it { should resemble '<span class="inline_math">' }
+    end
+
+    context "TeX-style" do
+      let(:equation) do <<-'EOS'
+        $\int_\Omega d\omega = \int_{\partial\Omega} \omega$
+        EOS
+      end
+      let(:polytex) { equation }
+      let(:contents) { '\Omega' }
+
+      it { should resemble contents }
+      it { should resemble '<span class="inline_math">' }
+      it { should resemble '\(' }
+    end
+  end
+
+  describe "multiple occurrences of inline math on one line" do
+    let(:polytex) { '$\Omega > 0$ and \( x^2 - 2 \equiv 0 \) should work.' }
+
+    it { should resemble '\Omega' }
+    it { should resemble '\equiv' }
+    it { should resemble '<span class="inline_math">' }
+    it { should resemble '\(' }
+  end
+
+  describe "display math" do
+
+    context "LaTeX-style" do
+      let(:equation) do <<-'EOS'
+        \[ \int_\Omega d\omega = \int_{\partial\Omega} \omega \]
+        EOS
+      end
+      let(:polytex) { "lorem\n" + equation + "\nipsum" }
+      let(:contents) { '\Omega' }
+
+      it { should resemble contents }
+      it { should resemble '<div class="display_math">' }
+    end
+
+    context "TeX-style" do
+      let(:equation) do <<-'EOS'
+        $$ \int_\Omega d\omega = \int_{\partial\Omega} \omega $$
+        EOS
+      end
+      let(:polytex) { "lorem\n" + equation + "\nipsum" }
+      let(:contents) { '\Omega' }
+
+      it { should resemble contents }
+      it { should resemble '<div class="display_math">' }
+    end
+  end
+
+  describe "equation environments" do
+
+    shared_examples "an equation environment" do
+      it { should resemble contents }
+      it { should resemble '<div class="equation">' }
+    end
+
+    context "alone" do
+      let(:equation) do <<-'EOS'
+        \begin{equation}
+        \int_\Omega d\omega = \int_{\partial\Omega} \omega
+        \end{equation}
+      EOS
+      end
+      let(:polytex) { equation }
+      let(:contents) { equation }
+
+      it_behaves_like "an equation environment"
+    end
+
+    context "surrounded by text" do
+      let(:equation) do <<-'EOS'
+        \begin{equation}
+        \int_\Omega d\omega = \int_{\partial\Omega} \omega
+        \end{equation}
+        EOS
+      end
+      let(:polytex) { "lorem\n" + equation + "\nipsum" }
+      let(:contents) { equation }
+
+      it_behaves_like "an equation environment"
+      it { should resemble '<p>lorem' }
+      it { should resemble '<p class="noindent">ipsum' }
+    end
+
+  context "align" do
+      let(:equation) do <<-'EOS'
+        \begin{align}
+        x^2 + y^2 & = 1 \\
+        y & = \sqrt{1 - x^2}.
+        \end{align}
+        EOS
+      end
+      let(:polytex) { equation }
+      let(:contents) do <<-'EOS'
+        \begin{align}
+        x^2 + y^2 &amp; = 1 \\
+        y &amp; = \sqrt{1 - x^2}.
+        \end{align}
+        EOS
+      end
+
+      it_behaves_like "an equation environment"
+    end
+
+    context "align*" do
+      let(:equation) do <<-'EOS'
+        \begin{align*}
+        x^2 + y^2 & = 1 \\
+        y & = \sqrt{1 - x^2}.
+        \end{align*}
+        EOS
+      end
+      let(:polytex) { equation }
+      let(:contents) do <<-'EOS'
+        \begin{align*}
+        x^2 + y^2 &amp; = 1 \\
+        y &amp; = \sqrt{1 - x^2}.
+        \end{align*}
+      EOS
+      end
+
+      it_behaves_like "an equation environment"
+    end
+
+    context "aligned" do
+      let(:equation) do <<-'EOS'
+        \begin{equation}
+        \begin{aligned}
+        \nabla \times \vec{\mathbf{B}} -\, \frac1c\,
+        \frac{\partial\vec{\mathbf{E}}}{\partial t} & =
+        \frac{4\pi}{c}\vec{\mathbf{j}} \\   \nabla \cdot \vec{\mathbf{E}} & =
+        4 \pi \rho \\
+        \nabla \times \vec{\mathbf{E}}\, +\, \frac1c\,
+        \frac{\partial\vec{\mathbf{B}}}{\partial t} & = \vec{\mathbf{0}} \\
+        \nabla \cdot \vec{\mathbf{B}} & = 0
+        \end{aligned}
+        \end{equation}
+        EOS
+      end
+      let(:polytex) { equation }
+      let(:contents) do <<-'EOS'
+        \begin{aligned}
+        \nabla \times \vec{\mathbf{B}} -\, \frac1c\,
+        \frac{\partial\vec{\mathbf{E}}}{\partial t} &amp; =
+        \frac{4\pi}{c}\vec{\mathbf{j}} \\   \nabla \cdot \vec{\mathbf{E}} &amp;
+        = 4 \pi \rho \\
+        \nabla \times \vec{\mathbf{E}}\, +\, \frac1c\,
+        \frac{\partial\vec{\mathbf{B}}}{\partial t} &amp; = \vec{\mathbf{0}} \\
+        \nabla \cdot \vec{\mathbf{B}} &amp; = 0
+        \end{aligned}
+        EOS
+      end
+
+      it_behaves_like "an equation environment"
+    end
+
+    context "equation* with nesting" do
+      let(:equation) do <<-'EOS'
+        \begin{equation*}
+        \left.\begin{aligned}
+        dE  &= \rho \\
+        d*B &= J + \dot{E}
+        \end{aligned}
+        \right\}
+        \qquad \text{Maxwell}
+        \end{equation*}
+        EOS
+      end
+      let(:polytex) { equation }
+      let(:contents) do <<-'EOS'
+        \begin{equation*}
+        \left.\begin{aligned}
+        dE  &amp;= \rho \\
+        d*B &amp;= J + \dot{E}
+        \end{aligned}
+        \right\}
+        \qquad \text{Maxwell}
+        \end{equation*}
+        EOS
+      end
+
+      it_behaves_like "an equation environment"
+    end
+  end
+end
