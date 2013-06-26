27aea2f2463f09834b5eb3c72e4f939a7b5b4c7a
Michael Hartl <michael@michaelhartl.com>
2013-06-25 18:28:10 -0700

    Write proper failing test for eq xrefs
    [#52357073]

---
+++ spec/to_html/eqref_spec.rb
@@ -0,0 +1,31 @@
+# encoding=utf-8
+require 'spec_helper'
+
+describe Polytexnic::Core::Pipeline do
+  let(:processed_text) { Polytexnic::Core::Pipeline.new(polytex).to_html }
+  subject { processed_text }
+
+  describe 'equation \ref' do
+
+      let(:equation) do <<-'EOS'
+        \chapter{Equation test}
+
+        \begin{equation}
+        \label{eq:foobar}
+        x
+        \end{equation}
+
+       EOS
+    end
+
+    context 'with \ref{eq:foobar}' do
+      let(:polytex) { equation + '\ref{eq:foobar}' }
+      it { should include '1.1' }
+    end
+
+    context 'with \eqref{eq:foobar}' do
+      let(:polytex) { equation + '\ref{eq:foobar}' }
+      it { should include '(1.1)' }
+    end
+  end
+end
