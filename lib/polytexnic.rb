e6afeb8273669e245a1a5ba2e47af7ebdb3f7e17
Michael Hartl <michael@michaelhartl.com>
2023-09-03 14:40:30 -0700

    Remove 'note' theorem type, which is incompatible with the system somehow

--- lib/polytexnic.rb
+++ lib/polytexnic.rb
@@ -52,7 +52,7 @@ module Polytexnic
       @supported_theorem_types = %w[theorem lemma corollary proposition
         conjecture criterion assertion definition condition problem example
         exercise algorithm question axiom property assumption hypothesis
-        remark note notation claim summary acknowledgment case conclusion]
+        remark notation claim summary acknowledgment case conclusion]
       @language_labels = if (labels = options[:language_labels]).nil?
                             default_language_labels
                           else
