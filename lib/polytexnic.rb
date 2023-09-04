b083b34f594dfde7fae5e54270abbcbb133dc546
Michael Hartl <michael@michaelhartl.com>
2023-09-04 10:06:17 -0700

    Add support for more definition-like environments

--- lib/polytexnic.rb
+++ lib/polytexnic.rb
@@ -50,7 +50,9 @@ module Polytexnic
       @maketitle_elements = {}
       @article = options[:article]
       @supported_theorem_types = %w[theorem lemma corollary proposition
-                                    conjecture definition axiom remark claim]
+                                    conjecture
+                                    definition problem example exercise axiom
+                                    remark claim]
       @language_labels = if (labels = options[:language_labels]).nil?
                             default_language_labels
                           else
